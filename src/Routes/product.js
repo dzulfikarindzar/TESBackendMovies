919da008446e71dcced417a71d493cba2ff13554
dzulfikarindzar <72298156+dzulfikarindzar@users.noreply.github.com>
2020-12-08 14:43:45 +0700

    Update endpoint product

--- src/Routes/product.js
+++ src/Routes/product.js
@@ -3,12 +3,10 @@ const routes = express.Router()
 const ctrl = require("../Controllers/product")
 
 
-
-// /product
-routes.get('/', ctrl.get)
-routes.get('/:name', ctrl.getName)
+routes.get('/', ctrl.getAll)
+routes.get('/:id', ctrl.get)
 routes.post("/", ctrl.add)
 routes.put("/", ctrl.update)
-routes.delete("/:idprod", ctrl.del)
+routes.delete("/:id", ctrl.del)
 
 module.exports = routes
