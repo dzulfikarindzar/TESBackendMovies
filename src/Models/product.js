8c9d77a8e2709f9fd153c5b6efd50757c9e19666
dzulfikarindzar <72298156+dzulfikarindzar@users.noreply.github.com>
2020-12-08 14:58:57 +0700

    Add Join, Sort, and orderBy

--- src/Models/product.js
+++ src/Models/product.js
@@ -2,37 +2,89 @@ const db = require('../Config/db')
 const products = {}
 
 
-products.get = () =>{
-    return new Promise((resolve, reject) =>{
-        db.query("SELECT * FROM public.product ORDER BY price DESC")
+products.getAll= () => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        "SELECT product.id, product.name,  product.image, product.price, category.name AS category FROM public.product LEFT JOIN public.category ON category.id = product.id_category ORDER BY product.id ASC")
         .then((res) => {
-            if (res.rows.length == 0) {
-                resolve({msg : "Data Kosong"})
-            } else {
-                resolve(res.rows)
-            }
+          if (res.rows.length == 0) {
+            resolve('Data pada tabel Kosong!');
+          } else {
+            resolve(res.rows);
+          }
         })
         .catch((err) => {
-            reject(err)
+          reject(err);
+        });
+    });
+  }
+  products.getSearch= (name) => {
+    return new Promise((resolve, reject) => {
+      db.query( `SELECT product.id, product.name,  product.image, product.price, category.name AS category FROM public.product LEFT JOIN public.category ON category.id = product.id_category WHERE product.name ILIKE '%${name}%'`)
+        .then((res) => {
+          if (res.rows.length == 0) {
+            resolve('Data di tabel Kosong!');
+          } else {
+            resolve(res.rows);
+          }
         })
-    })
-}
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  products.getSort= (order, sort) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT product.id, product.name,  product.image, product.price, category.name AS category FROM public.product LEFT JOIN public.category ON category.id = product.id_category ORDER BY ${order} ${sort}`,
+      )
+        .then((res) => {
+          if (res.rows.length == 0) {
+            resolve('Data di tabel kosong!');
+          } else {
+            resolve(res.rows);
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  products.get= (id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT product.id, product.name,  product.image, product.price, category.name AS category FROM public.product LEFT JOIN public.category ON category.id = product.id_category WHERE product.id=${id}`,
+      )
+        .then((res) => {
+          if (res.rows.length == 0) {
+            resolve('Data di tabel kosong!');
+          } else {
+            resolve(res.rows);
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
 
 products.addProd = (data) =>{
     return new Promise((resolve, reject) =>{
-        db.query(`INSERT INTO public.product(name, image, price, category) VALUES ('${data.name}', '${data.image}', ${data.price}, '${data.category}')`)
+        db.query(`INSERT INTO public.product(name, image, price, id_category) VALUES ('${data.name}', '${data.image}', ${data.price}, ${data.id_category})`)
         .then((res) => {
             resolve(data)
         })
         .catch((err) => {
-            reject(err)
+            reject("Input data harus lengkap!")
         })
     })
 }
 
 products.updateProd = (data) =>{
     return new Promise((resolve, reject) =>{
-        db.query(`UPDATE public.product SET name='${data.name}', image='${data.image}', price=${data.price}, category='${data.category}' WHERE idprod=${data.idprod}`)
+        db.query(`UPDATE public.product SET name='${data.name}', image='${data.image}', price=${data.price}, id_category='${data.id_category}' WHERE id=${data.id}`)
         .then((res) => {
             resolve(data)
         })
@@ -45,29 +97,15 @@ products.updateProd = (data) =>{
 
 products.delProd = (id) =>{
     return new Promise((resolve, reject) =>{
-        db.query(`DELETE FROM public.product WHERE idprod=${id}`)
+        db.query(`DELETE FROM public.product WHERE id=${id}`)
         .then((res) => {
-            resolve(id)
+            resolve("Data Sukses dihapus!")
         })
         .catch((err) => {
             reject(err)
         })
     })
 }
-
-
-products.getName = (data) =>{
-    return new Promise((resolve, reject) =>{
-        db.query(`SELECT * FROM public.product WHERE name LIKE '${data}%'`)
-        .then((res) => {
-            resolve(res.rows)
-        })
-        .catch((err) => {
-            reject(err)
-        })
-    })
-}
-
 
 module.exports = products
 
